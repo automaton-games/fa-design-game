860138bafb15f8d0351fe3eac1a3f8bd69fdb81d
ymn2525 <e255725@ie.u-ryukyu.ac.jp>
2026-07-09 09:37:24 +0900

    Ginを使ってHelathAPIをリファクタリングした

--- backend/main.go
+++ backend/main.go
@@ -1,44 +1,27 @@
 package main
 
 import (
-	"encoding/json" // JSON形式でレスポンスを返すため
-	"log"           // サーバー起動時のログ出力
 	"net/http"      // HTTPサーバーを構築するため
+	"github.com/gin-gonic/gin" // ginです。webフレームワーク
 )
 
-// HealthResponse は/health APIのレスポンスを表す構造体
 type HealthResponse struct {
-	Status string `json:"status"` // JSONでは"status"というキーで返す
+	Status string `json:"status"`
 }
 
-// healthHandler は GET /health のリクエストを処理するハンドラー
-func healthHandler(w http.ResponseWriter, r *http.Request) {
-
-	// GETメゾッド以外のアクセスは受け付けない
-	if r.Method != http.MethodGet {
-		http.Error(w, "Method Not Allowed", http.StatusMethodNotAllowed)
-		return
-	}
-	// レスポンスをJSON形式で返すことを宣言
-	w.Header().Set("Content-Type", "application/json")
-
-	// クライアントへ返すデータを作成
-	response := HealthResponse{
+// HealthAPIの実装
+func healthHandler(c *gin.Context) {
+	c.Header("Content-Type", "application/json; charset=utf-8")
+	c.JSON(http.StatusOK, HealthResponse{
 		Status: "ok",
-	}
-	// responseをJSON形式に変換してレスポンスとして送信
-	json.NewEncoder(w).Encode(response)
+	})
 }
 
 func main() {
+  router := gin.Default()
+	
+	// GETメソッドかつ/health にリクエストが来たらこのルーティングが実行されて、healthHandlerが呼び出される
+  router.GET("/health", healthHandler)
 
-	// "/health"にアクセスされたらhealthHandlerを実行する
-	http.HandleFunc("/health", healthHandler)
-
-	// サーバー起動時のログ
-	log.Println("Server started on :8080")
-
-	// ポート8080でHTTPサーバーを起動
-	// ListenAndServeはエラーが発生するまで処理を続ける
-	log.Fatal(http.ListenAndServe(":8080", nil))
+  router.Run() // デフォルトで0.0.0.0:8080でリッスンします
 }
